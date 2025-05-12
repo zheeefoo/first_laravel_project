42dcafae81b954d0b6aacfe396e4f28f1c640142
foozheee <jobfoozheee@gmail.com>
2025-05-12 22:37:01 +0800

    updates

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -154,11 +154,12 @@
 
         <header class="bg-white shadow-sm">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{$heading}}</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                {{ $slot }}
                 <!-- Your content -->
             </div>
         </main>
--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,17 +1,17 @@
-@props(['active' => false, 'version' => 'd'])
+@props(['active' => false, 'version' => 'd', 'type' => 'a'])
 
 @php
     $active_class = 'bg-gray-900 text-white ';
     $inactive_class = 'text-gray-300 hover:bg-gray-700 hover:text-white';
     if ($version == 'd') {
         $add_on = 'rounded-md px-3 py-2 text-sm font-medium';
-    } else if ($type == 'm') {
+    } else if ($version == 'm') {
         //$active_class = 'bg-gray-900 text-white';
         //$inactive_class = 'text-gray-300 hover:bg-gray-700 hover:text-white';
         $add_on = 'px-3 py-2 text-base font-medium block rounded-md';
     }
 @endphp
-<?php if ($type == 'a'): ?>
+<?php  if ($type == 'a'): ?>
 <a class="{{ $active ? $active_class : $inactive_class}} {{ $add_on }}" aria-current="{{ $active ? 'page' : 'false'}}"
     {{ $attributes }}>
     {{ $slot }}</a>
@@ -23,12 +23,12 @@
 
 <!-- or -->
 <!-- blade format -->
-<!-- @if ($type == 'a')
-    <a class="{{ $active ? $active_class : $inactive_class}} {{ $add_on }}" aria-current="{{ $active ? 'page' : 'false'}}"
-        {{ $attributes }}>
-        {{ $slot }}</a>
+{{-- @if ($type == 'a')
+<a class="{{ $active ? $active_class : $inactive_class}} {{ $add_on }}" aria-current="{{ $active ? 'page' : 'false'}}"
+    {{ $attributes }}>
+    {{ $slot }}</a>
 @elseif ($type == 'b')
-    <button class="{{ $active ? $active_class : $inactive_class}} {{ $add_on }}"
-        aria-current="{{ $active ? 'page' : 'false'}}" {{ $attributes }}>
-        {{ $slot }}</button>
-@endif -->
+<button class="{{ $active ? $active_class : $inactive_class}} {{ $add_on }}"
+    aria-current="{{ $active ? 'page' : 'false'}}" {{ $attributes }}>
+    {{ $slot }}</button>
+@endif --}}
